07488eda3314aa54530c2234fc8ca4723d4417a4
yashasr498-a11y <yashasr498@gmail.com>
2025-11-02 12:35:52 +0530

    Create page.tsx

---
+++ crafted-wear/app/page.tsx
@@ -0,0 +1,29 @@
++import Hero from "@/components/Hero";
+
++import Products from "@/components/Products";
+
++import About from "@/components/About";
+
++import Footer from "@/components/Footer";
+
++
+
++export default function Home() {
+
++  return (
+
++    <main className="min-h-screen">
+
++      <Hero />
+
++      <Products />
+
++      <About />
+
++      <Footer />
+
++    </main>
+
++  );
+
++}
